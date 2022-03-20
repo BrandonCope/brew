727b4b74431961e98d981964a50cad8f6869a03b
brandon <bcopeland97@gmail.com>
2022-03-20 15:13:29 -0400

    updated review db w text datatype

--- app/models/review.py
+++ app/models/review.py
@@ -5,7 +5,7 @@ class Review(db.Model):
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     brewery_id = db.Column(db.Integer, db.ForeignKey("breweries.id"), nullable=False)
