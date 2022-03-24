1de9fef96e362332f230cf0aa1547c12cb966c02
brandon <bcopeland97@gmail.com>
2022-03-24 12:19:35 -0400

    404 update

--- react-app/src/components/NotFound/index.js
+++ react-app/src/components/NotFound/index.js
@@ -6,8 +6,8 @@ const PageNotFound = () => {
 
     return (
         <div className="body-div">
-           <h1 className="not-found-message">Page Not Found: 404</h1>
-           {/* <img className="homer-quote" alt="Homer-quote" src="https://brew-aa.s3.amazonaws.com/36ec86adfb4c4e04b45bf2ba4ddb012f.jpg" /> */}
+           <h1 className="not-found-message">Page Not Found: Maybe Homer can help.</h1>
+           <img className="homer-quote" alt="Homer-quote" src="https://brew-aa.s3.amazonaws.com/36ec86adfb4c4e04b45bf2ba4ddb012f.jpg" />
         </div>
     )
 }
